28758e3ed59818de4c6f72b0f9932fdcce3cf853
Sultan Alsamaani <ssamaani@qumc.edu.sa>
2020-01-20 10:54:44 +0300

    first commit

---
+++ BlazorApp1/Data/SchoolDbContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorApp1.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace BlazorApp1.Data
+{
+    public class SchoolDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        public SchoolDbContext(DbContextOptions<SchoolDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Student>().HasData(
+              new
+              {
+                  StudentId = Guid.NewGuid().ToString(),
+                  FirstName = "Jane",
+                  LastName = "Smith",
+                  School = "Medicine"
+              }, new
+              {
+                  StudentId = Guid.NewGuid().ToString(),
+                  FirstName = "John",
+                  LastName = "Fisher",
+                  School = "Engineering"
+              }, new
+              {
+                  StudentId = Guid.NewGuid().ToString(),
+                  FirstName = "Pamela",
+                  LastName = "Baker",
+                  School = "Food Science"
+              }, new
+              {
+                  StudentId = Guid.NewGuid().ToString(),
+                  FirstName = "Peter",
+                  LastName = "Taylor",
+                  School = "Mining"
+              }
+            );
+        }
+    }
+}
---
+++ BlazorApp1/Migrations/20200119092043_initial_migration.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlazorApp1.Migrations
+{
+    public partial class initial_migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    StudentId = table.Column<string>(nullable: false),
+                    FirstName = table.Column<string>(nullable: false),
+                    LastName = table.Column<string>(nullable: false),
+                    School = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.StudentId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "StudentId", "FirstName", "LastName", "School" },
+                values: new object[,]
+                {
+                    { "8f8e054d-1ca4-4ee5-984d-50124eea2a60", "Jane", "Smith", "Medicine" },
+                    { "0a3ebb38-ca8b-40b4-b895-39a920e69f25", "John", "Fisher", "Engineering" },
+                    { "fb347b32-0828-4928-b615-a3414778d719", "Pamela", "Baker", "Food Science" },
+                    { "9976a1d7-5bc0-4fc3-b87d-7d08c729fa76", "Peter", "Taylor", "Mining" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
